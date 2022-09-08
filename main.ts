236dc2b1d6a695ad835f62962f4220b0b3020d51
Kyo <112098041+Breckin-Deverman@users.noreply.github.com>
2022-09-08 10:29:46 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let answer = 0
+let Yon = 0
+let Three = 0
+let zwei = 0
+let いち = 0
+let tempC = randint(-30, 200)
+let tempF = tempC * 1.8
+let tempF2 = tempF + 32
+game.splash("" + tempC + "C=" + ("" + tempF2 + "F"))
+for (let index = 0; index < 99999999999999; index++) {
+    いち = Math.round(668758937) * (Math.abs(Math.sqrt(50)) % randint(9, 999999))
+    zwei = 64
+    Three = 790979838795808
+    Yon = Math.round(97682358) * (Math.abs(Math.sqrt(38)) % randint(96, 999999))
+    answer = randint(いち, randint(zwei * Yon, Three))
+    game.splash("Answer = " + answer)
+}
